8df768456563add56ac961dc99d0f229ab2485de
Peter Hegman <phegman@unionstmedia.com>
2016-10-17 22:30:29 -0400

    fixing issue where exception is thrown if there are now slideshows created

--- components/Slider.php
+++ components/Slider.php
@@ -2,21 +2,30 @@
 
 use peterhegman\slickslider\models\SlideShows;
 use Symfony\Component\HttpFoundation\Request;
+use Log;
 
 class Slider extends \Cms\Classes\ComponentBase
 {
     public function componentDetails()
     {
-        return [
-            'name' => 'Slider',
-            'description' => 'Displays a slider choosen from the dropdown'
-        ];
+        $slideShows = SlideShows::all();
+        if (count($slideShows) > 0) {
+            return [
+                'name' => 'Slider',
+                'description' => 'Displays a slider choosen from the dropdown'
+            ];
+        } else {
+            return [
+                'name' => 'Slider',
+                'description' => 'Please create a slideshow in the "Slideshow" menu'
+            ];
+        }
     }
 
     public function defineProperties()
     {
         $slideShows = SlideShows::all();
-        if (!$slideShows->isEmpty()) {
+        if (count($slideShows) > 0) {
             $optionsArray =  array();
             foreach ($slideShows as $slideShow) {
                 $optionsArray[$slideShow->attributes['id']] = $slideShow->attributes['slide_show_title'];
@@ -26,9 +35,12 @@ class Slider extends \Cms\Classes\ComponentBase
                     'title'       => 'Slideshow',
                     'type'        => 'dropdown',
                     'default'     => $slideShows->first()->attributes['id'],
-                    'placeholder' => 'Select units',
+                    'placeholder' => 'Select slideshow',
                     'options'     => $optionsArray
                 ]
+            ];
+        } else {
+            return [
             ];
         }
     }
@@ -46,19 +58,25 @@ class Slider extends \Cms\Classes\ComponentBase
     {
         $slideShowId = $this->property('slide_show_id');
         $slideShows = SlideShows::where('id', '=', $slideShowId)->first();
-        $breakpoints = json_decode($slideShows->attributes['responsive']);
-        //Create Responsive Array
-        $breakpointArray = array();
-        $i = 0;
-        foreach ($breakpoints as $breakpoint) {
-            $breakpointArray[$i]['breakpoint'] = (int)$breakpoint->breakpoint;
-            $breakpointArray[$i]['settings']['slidesToShow'] = ( $breakpoint->responsive_slides_to_show ? (int)$breakpoint->responsive_slides_to_show : 1 );
-            $breakpointArray[$i]['settings']['slidesToScroll'] = ( $breakpoint->responsive_slides_to_scroll ? (int)$breakpoint->responsive_slides_to_scroll : 1 );
-            $breakpointArray[$i]['settings']['rows'] = ( $breakpoint->responsive_rows ? (int)$breakpoint->responsive_rows : 1 );
-            $breakpointArray[$i]['settings']['slidesPerRow'] = ( $breakpoint->responsive_slides_per_row ? (int)$breakpoint->responsive_slides_per_row : 1 );
-            $i++;
+        if (count($slideShows) > 0) {
+            Log::debug(count($slideShows));
+            $breakpoints = json_decode($slideShows->attributes['responsive']);
+            //Create Responsive Array
+            $breakpointArray = array();
+            $i = 0;
+            foreach ($breakpoints as $breakpoint) {
+                $breakpointArray[$i]['breakpoint'] = (int)$breakpoint->breakpoint;
+                $breakpointArray[$i]['settings']['slidesToShow'] = ( $breakpoint->responsive_slides_to_show ? (int)$breakpoint->responsive_slides_to_show : 1 );
+                $breakpointArray[$i]['settings']['slidesToScroll'] = ( $breakpoint->responsive_slides_to_scroll ? (int)$breakpoint->responsive_slides_to_scroll : 1 );
+                $breakpointArray[$i]['settings']['rows'] = ( $breakpoint->responsive_rows ? (int)$breakpoint->responsive_rows : 1 );
+                $breakpointArray[$i]['settings']['slidesPerRow'] = ( $breakpoint->responsive_slides_per_row ? (int)$breakpoint->responsive_slides_per_row : 1 );
+                $i++;
+            }
+            $breakpointJson = json_encode($breakpointArray);
+            return compact('slideShows', 'breakpointJson');
+        } else {
+            $slideShows = 'no_slider';
+            return compact('slideShows');
         }
-        $breakpointJson = json_encode($breakpointArray);
-        return compact('slideShows', 'breakpointJson');
     }
 }
